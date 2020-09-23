c3df22707d8bd49354f0fbcddfd1ee0f9766b2f8
Wolf McNally <wolf@wolfmcnally.com>
2020-09-23 01:55:03 -0700

    Fixed broken build.
    Fixed some esoteric problems with xcodebuild installs and XCFramework creation not copying all the necessary files in the framework to the build product.

---
+++ BCLibsSwiftTest/BCLibsSwiftTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BCLibsSwiftTest
+//
+//  Created by Wolf McNally on 9/22/20.
+//
+
+import UIKit
+
+import CCryptoBase
+//import CShamir
+//import CSSKR
+
+import CryptoBase
+//import Shamir
+//import SSKR
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- build.sh
+++ build.sh
@@ -159,35 +159,48 @@ build_swift_framework()
   XC_ARCH=$2
   XC_BUILD_DIR_NAME=$3
   XC_SDK=$4
-  XC_CATALYST=$5
-  XC_VERSION=$6
+  XC_PLATFORM_DIR=$5
+  XC_CATALYST=$6
+  XC_VERSION=$7
+  XC_CONFIGURATION=Debug
 
   FRAMEWORK_ROOT=${PROJ_ROOT}/${XC_FRAMEWORK}
 
   XC_PROJECT=${FRAMEWORK_ROOT}/${XC_FRAMEWORK}.xcodeproj
   XC_SCHEME=${XC_FRAMEWORK}
-  XC_BUILD_DIR=${BUILD_ROOT}/${XC_BUILD_DIR_NAME}
-  XC_ARCHIVE_PATH=${XC_BUILD_DIR}/${XC_FRAMEWORK}.xcarchive
-  rm -rf ${ARCHIVE_PATH}
-  xcodebuild clean archive \
+  XC_DEST_BUILD_DIR=${BUILD_ROOT}/${XC_BUILD_DIR_NAME}
+  XC_FRAMEWORK_DIR_NAME=${XC_FRAMEWORK}.framework
+  rm -rf ${XC_DEST_BUILD_DIR}/${XC_FRAMEWORK_DIR_NAME}
+
+  XC_ARGS="\
     -project ${XC_PROJECT} \
     -scheme ${XC_SCHEME} \
-    -archivePath ${XC_ARCHIVE_PATH} \
+    -configuration ${XC_CONFIGURATION} \
     -sdk ${XC_SDK} \
     ${XC_VERSION} \
     ONLY_ACTIVE_ARCH=YES \
     ARCHS=${XC_ARCH} \
     SKIP_INSTALL=NO \
     BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
-    SUPPORTS_MACCATALYST=${XC_CATALYST} \
-    FRAMEWORK_SEARCH_PATHS="${BUILD_ROOT}/"
+    SUPPORTS_MACCATALYST=${XC_CATALYST}"
+
+  xcodebuild clean build ${XC_ARGS[@]}
+
+  XC_BUILD_DIR=`
+    xcodebuild ${XC_ARGS[@]} -showBuildSettings | grep -o '\<BUILD_DIR = .*' | cut -d ' ' -f 3
+    `
+
+  XC_FRAMEWORK_SOURCE_DIR=${XC_BUILD_DIR}/${XC_CONFIGURATION}-${XC_PLATFORM_DIR}
+  cp -R "${XC_FRAMEWORK_SOURCE_DIR}/${XC_FRAMEWORK_DIR_NAME}" ${XC_DEST_BUILD_DIR}/
+
+  echo diff -rq "${XC_FRAMEWORK_SOURCE_DIR}/${XC_FRAMEWORK_DIR_NAME}" "${XC_DEST_BUILD_DIR}/${XC_FRAMEWORK_DIR_NAME}"
 }
 
 build_swift_frameworks()
 (
-  IOS_ARM64_PARAMS=("arm64" "ios-arm64" "iphoneos" "NO" "IPHONEOS_DEPLOYMENT_TARGET=${MIN_IOS_VERSION}")
-  MAC_CATALYST_X86_64_PARAMS=("x86_64" "mac-catalyst-x86_64" "macosx" "YES" "MACOSX_DEPLOYMENT_TARGET=${MIN_MAC_VERSION}")
-  IOS_SIMULATOR_X86_64_PARAMS=("x86_64" "ios-simulator-x86_64" "iphonesimulator" "NO" "IPHONEOS_DEPLOYMENT_TARGET=${MIN_IOS_VERSION}")
+  IOS_ARM64_PARAMS=("arm64" "ios-arm64" "iphoneos" "iphoneos" "NO" "IPHONEOS_DEPLOYMENT_TARGET=${MIN_IOS_VERSION}")
+  MAC_CATALYST_X86_64_PARAMS=("x86_64" "mac-catalyst-x86_64" "macosx" "maccatalyst" "YES" "MACOSX_DEPLOYMENT_TARGET=${MIN_MAC_VERSION}")
+  IOS_SIMULATOR_X86_64_PARAMS=("x86_64" "ios-simulator-x86_64" "iphonesimulator" "iphonesimulator" "NO" "IPHONEOS_DEPLOYMENT_TARGET=${MIN_IOS_VERSION}")
 
   build_swift_framework CryptoBase ${IOS_ARM64_PARAMS[@]}
   build_swift_framework CryptoBase ${MAC_CATALYST_X86_64_PARAMS[@]}
@@ -204,16 +217,31 @@ build_swift_frameworks()
 
 build_swift_xcframework()
 {
-  XC_FRAMEWORK=$1
+  FRAMEWORK_NAME=$1
 
-  FRAMEWORK_PATH=${XC_FRAMEWORK}.xcarchive/Products/Library/Frameworks/${XC_FRAMEWORK}.framework
+  PLATFORM_FRAMEWORK_NAME=${FRAMEWORK_NAME}.framework
+  XC_FRAMEWORK_NAME=${FRAMEWORK_NAME}.xcframework
+  XC_FRAMEWORK_PATH=${BUILD_ROOT}/${XC_FRAMEWORK_NAME}
 
-  rm -rf ${BUILD_ROOT}/${XC_FRAMEWORK}.xcframework
+  rm -rf ${XC_FRAMEWORK_PATH}
   xcodebuild -create-xcframework \
-  -framework ${BUILD_ROOT}/ios-arm64/${FRAMEWORK_PATH} \
-  -framework ${BUILD_ROOT}/mac-catalyst-x86_64/${FRAMEWORK_PATH} \
-  -framework ${BUILD_ROOT}/ios-simulator-x86_64/${FRAMEWORK_PATH} \
-  -output ${BUILD_ROOT}/${XC_FRAMEWORK}.xcframework
+  -framework ${BUILD_ROOT}/ios-arm64/${PLATFORM_FRAMEWORK_NAME} \
+  -framework ${BUILD_ROOT}/mac-catalyst-x86_64/${PLATFORM_FRAMEWORK_NAME} \
+  -framework ${BUILD_ROOT}/ios-simulator-x86_64/${PLATFORM_FRAMEWORK_NAME} \
+  -output ${XC_FRAMEWORK_PATH}
+
+  # As of September 22, 2020, the step above is broken:
+  # it creates unusable XCFrameworks; missing files like Modules/CryptoBase.swiftmodule/Project/x86_64-apple-ios-simulator.swiftsourceinfo
+  # The frameworks we started with were fine. So we're going to brute-force replace the frameworks in the XCFramework with the originials.
+
+  rm -rf ${XC_FRAMEWORK_PATH}/ios-arm64/${PLATFORM_FRAMEWORK_NAME}
+  cp -R ${BUILD_ROOT}/ios-arm64/${PLATFORM_FRAMEWORK_NAME} ${XC_FRAMEWORK_PATH}/ios-arm64/
+
+  rm -rf ${XC_FRAMEWORK_PATH}/ios-x86_64-maccatalyst/${PLATFORM_FRAMEWORK_NAME}
+  cp -R ${BUILD_ROOT}/mac-catalyst-x86_64/${PLATFORM_FRAMEWORK_NAME} ${XC_FRAMEWORK_PATH}/ios-x86_64-maccatalyst/
+
+  rm -rf ${XC_FRAMEWORK_PATH}/ios-x86_64-simulator/${PLATFORM_FRAMEWORK_NAME}
+  cp -R ${BUILD_ROOT}/ios-simulator-x86_64/${PLATFORM_FRAMEWORK_NAME} ${XC_FRAMEWORK_PATH}/ios-x86_64-simulator/
 }
 
 build_swift_xcframeworks()
